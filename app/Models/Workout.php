2aa1bf119098ffae96225bf31c8359b3d3a2110e
dochinhthtn <dochinhthtn@gmail.com>
2020-10-11 21:36:54 +0700

    Make models

---
+++ app/Models/Workout.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Workout extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function exercises() {
+        return $this->belongsToMany(Exercise::class, 'workouts_exercises', 'workout_id', 'exercise_id');
+    }
+}
