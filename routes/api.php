fa290da8a3da9de0be614a4e1c1a6a708b1bb51c
dochinhthtn <dochinhthtn@gmail.com>
2020-11-01 22:29:18 +0700

    Add route for workout

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\ExerciseController;
 use App\Http\Controllers\API\PlanController;
-use App\Models\Plan;
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\WorkoutController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,22 +14,53 @@ use Illuminate\Support\Facades\Validator;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
+ */
 
 Route::get('/test', function () {
-    // Plan::create(['id' => 1000, 'name' => 'my first plan']);
+    // return response()->json([
+    //     'name' => 'my image collection',
+    //     'items' => [
+    //         ['name' => 'item 1', 'price' => 2, 'weight' => 3, 'damage' => 10],
+    //         ['name' => 'item 2', 'price' => 2, 'weight' => 3, 'treatment' => 1],
+    //         ['name' => 'item 3', 'price' => 2, 'weight' => 3, 'damage' => 2],
+    //         ['name' => 'item 4', 'price' => 2, 'weight' => 3, 'treatment' => 5],
+    //         ['name' => 'item 5', 'price' => 2, 'weight' => 3, 'damage' => 1],
+    //     ]
+    // ]);
 
-    // $plan = Plan::find(1);
-    // $plan->update(['name' => 'hahaha']);
+    return response()->json([
+        'name' => [
+            'firstName' => 'Chinh',
+            'lastName' => 'Do'
+        ],
+
+        'age' => 22,
+        'favorites' => [
+            ['type' => 'sport', 'name' => 'badminton'],
+            ['type' => 'music', 'name' => 'red music'],
+            ['type' => 'film', 'name' => 'horror film']
+        ]
+    ]);
 });
 
 Route::group(['prefix' => 'plan'], function () {
     Route::get('/', [PlanController::class, 'getPlans']);
     Route::get('/hint/{age}/{sex}/{bmi}', [PlanController::class, 'getPlansByHint']);
+    Route::get('/{plan:id}', [PlanController::class, 'getPlan']);
     Route::post('/', [PlanController::class, 'addPlan']);
     Route::put('/{plan:id}', [PlanController::class, 'updatePlan']);
 });
 
+Route::group(['prefix' => 'workout'], function () {
+    Route::get('/', [WorkoutController::class, 'getWorkouts']);
+    Route::get('/{workout:id}', [WorkoutController::class, 'getWorkout']);
+    Route::post('/', [WorkoutController::class, 'addWorkout']);
+    Route::put('/{workout:id}', [WorkoutController::class, 'updateWorkout']);
+});
 
-
+Route::group(['prefix' => 'exercise'], function() {
+    Route::get('/', [ExerciseController::class, 'getExercises']);
+    Route::get('/{exercise:id}', [ExerciseController::class, 'getExercise']);
+    Route::post('/', [ExerciseController::class, 'addExercise']);
+    Route::put('/{exercise:id}', [ExerciseController::class, 'updateExercise']);
+});
